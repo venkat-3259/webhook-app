1198a00a0cb26f73d7fccc610a1c70882119088c
Venkateshwara <118868784+venkat-3259@users.noreply.github.com>
2023-11-24 14:24:16 +0530

    Update handler.go

--- app/controller/handler.go
+++ app/controller/handler.go
@@ -3,21 +3,19 @@ package controller
 import (
 	"os"
 	"webhook/pkg/configs"
-	"webhook/pkg/worker"
 
 	"github.com/rs/zerolog"
 )
 
 type Handler struct {
-	config          *configs.Config
-	taskDistributor *worker.RedisTaskDistributor
-	logger          zerolog.Logger
+	config *configs.Config
+	logger zerolog.Logger
 }
 
 // OpenDBConnection func for opening database connection.
-func NewHandler(config *configs.Config, taskDistributor *worker.RedisTaskDistributor) *Handler {
+func NewHandler(config *configs.Config) *Handler {
 
 	logger := zerolog.New(os.Stderr).With().Timestamp().Str("Service", "API Handler").Logger()
 
-	return &Handler{config, taskDistributor, logger}
+	return &Handler{config, logger}
 }
